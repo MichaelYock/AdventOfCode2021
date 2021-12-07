eb3fcda0dee32e4dd224e166b044006553b5d26b
Michael Y <michael.yockney@gmail.com>
2021-12-08 01:16:40 +1300

    Day 2 Part 2, we're going deep

---
+++ 2-2/d2p2.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	inputFile := os.Args[1]
+	input, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	x := 0
+	y := 0
+	aim := 0
+
+	scanner := bufio.NewScanner(input)
+
+	for scanner.Scan() {
+		n := scanner.Text()
+		command := strings.Split(n, " ")
+		direction := command[0]
+		distance, err := strconv.Atoi(command[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		switch direction {
+		case "up":
+			aim -= distance
+		case "down":
+			aim += distance
+		case "forward":
+			x += distance
+			y += (aim * distance)
+		}
+	}
+	fmt.Println(x * y)
+}
